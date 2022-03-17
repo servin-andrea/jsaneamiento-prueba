adc3f06068b0b5af7db339850aff6a18e84593b5
Andrea Servin <andrea.servin@fiuni.edu.py>
2022-03-17 13:09:46 -0300

    add changes

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const helmet = require("helmet")
+const cors = require("cors")
+
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded( {extended:false }))
+app.use(helmet())
+app.use(cors())
+
+// routes
+app.use('/', (req, res) => {
+    res.send({
+        message: "Hello World!"
+    })
+})
+
+app.use('/products', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "product1"
+        },
+        {
+            id: 2,
+            name: "product2"
+        },
+        {
+            id: 3,
+            name: "product3"
+        }
+    ])
+})
+
+// initialize server
+app.listen(
+    app.get(4000), () => { 
+        console.log(`\nready - started server on http://localhost:4000\n`) 
+    }
+)
+
+module.exports = app
