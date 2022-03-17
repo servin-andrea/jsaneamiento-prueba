f8592aa452ace411ed54bcb265abc7e8de34c368
Andrea Servin <andrea.servin@fiuni.edu.py>
2022-03-16 22:00:56 -0300

    update index.js and app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 //settings
 app.set('port', config.PORT)
-syncDb()
+//syncDb()
 
 //middlewares
 app.use(express.json())
@@ -19,7 +19,7 @@ app.use(helmet())
 app.use(cors())
 
 //routes
-app.use('/api/barrios', barrioRoutes)
+//app.use('/api/barrios', barrioRoutes)
 app.use('/', (req, res) => {
     res.send({
         message: "Hello World!"
--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -2,9 +2,7 @@ import Barrios from "../resources/models/barrio.model";
 
 export const syncDb = async () => {
   try {
-    await Barrios.sync(/* { force: true }).then(() => {
-      console.log("Drop and re-sync db.");
-    } */);
+    await Barrios.sync();
   } catch (error) {
     console.log("\n\tOcurrió un error con la conexion a la BD\n", error.message);
   }
